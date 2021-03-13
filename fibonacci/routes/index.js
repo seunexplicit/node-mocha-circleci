7d28c55f3f4bc2b82625e7ad2fe821fad21616d4
seunexplicit <seunxplicit@gmail.com>
2021-03-13 19:46:57 +0100

    changes made to docker compose and index

--- fibonacci/routes/index.js
+++ fibonacci/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const fib = require('../module/fib');
+const fibSequence = require('../module/fibSequence');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(401).send('requires a value');
 });
+
+router.get('/:len', function(req, res){
+	let fibValue = fib.fib(req.params.len);
+	res.status(200).send(fibValue); 
+});
+
+router.sequence('/sequence/:seq', function(req, res){
+	let sequence = fibSequence(req.params.seq);
+	res.status(200).send(sequence);
+})
 
 module.exports = router;
